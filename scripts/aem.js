efafd1c80f3fe9d503b49ae0b8c8c3a075fa82c4
andrew-garcia-glob <andrew.garcia@globant.com>
2025-10-01 17:11:25 +0100

    Adds sections themes.

--- scripts/aem.js
+++ scripts/aem.js
@@ -505,6 +505,8 @@ function decorateSections(main) {
             .filter((style) => style)
             .map((style) => toClassName(style.trim()));
           styles.forEach((style) => section.classList.add(style));
+        } else if (key === 'theme') {
+          section.classList.add(meta.theme);
         } else {
           section.dataset[toCamelCase(key)] = meta[key];
         }
